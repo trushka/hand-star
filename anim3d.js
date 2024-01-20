f95cf2cef4855291101a91f8cd6c826ad34d4e30
trushka <trueshko@gmail.com>
2024-01-20 21:27:44 +0200

    Light width envMap, improved model

--- anim3d.js
+++ anim3d.js
@@ -1,4 +1,5 @@
-const model='./assets/hand3.glb'
+const model='./assets/hand3.glb',
+	envMap='./assets/map1.webp'
 
 import {THREE, vec3} from './threeCustom.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
@@ -20,26 +21,38 @@ const
 	scene = new THREE.Scene(),
 	material = new THREE.MeshStandardMaterial({
 		color: '#fff',
-		roughness: .17,
-		metalness: .997
+		roughness: .22,
+		metalness: .997,
+		emissive: new THREE.Color().multiplyScalar(-.08),
+		envMapIntensity: 1.3
 	}),
 	{PI, cos, sin}=Math,
 
 	lights = new THREE.Group(),
-	hLight = new THREE.HemisphereLight('#def', '#000', 15);
+	hLight = new THREE.AmbientLight( );
+	//hLight = new THREE.HemisphereLight('#def', '#000', 15);
 
-scene.add(hLight, lights);
+hLight.intencity=-1;
+scene.add(camera, lights)//hLight);
+
+new THREE.TextureLoader().load(envMap, tex=>{
+	scene.environment = new THREE.PMREMGenerator(renderer)
+	.fromEquirectangular(tex).texture
+}).colorSpace='srgb';
+
+//renderer.toneMapping = THREE.ACESFilmicToneMapping;
+
 hLight.position.set(0, -2.5, 3).normalize()
 
 RectAreaLightUniformsLib.init();
 
-addLight('#85f', 4, 120, 100, 0, 80, 10, -.3)
-addLight('#f3a', 38, 160, .5, 150, 70, 80, 1.7)
-addLight('#0ff', 190, 4, 20, 70, 80, -190, .3)
-addLight('#9f3', 100, 30, 15, 70, -80, 20, -.2)
-addLight('#f30', 20, 130, 15, -90, 30, -60,.3)
+addLight('#85f', 4, 120, 100, 0, 80, 10, 1.8)
+//addLight('#f3a', 38, 160, .5, 150, 70, 80, 5)
+addLight('#0ff', 360, 64, .8, 70, 80, -190, 2.5)
+//addLight('#af3', 100, 30, 15, 70, -80, 20, -.2)
+addLight('#f30', 30, 80, 15, -70, 10, -80,0)
 
-addLight('#adf', 290, 140, .1, -50, -220, -190, 0)
+addLight('#adf', 210, 140, .1, -100, -220, -190, 0)
 
 for (var i = 0; i < PI; i += PI/8) {
 	//addLight('#adf', 90, 2, 3, 0, 100*sin(i), 100*cos(i), .3)
@@ -50,7 +63,7 @@ document.body.prepend(canvas);
 
 camera.position.set(0, 25, 30);
 camera.lookAt(0,0,0);
-
+scene.rotation.set(-.5, -1.21, 0)
 
 
 let hand, arm, mesh; 
@@ -66,7 +79,7 @@ new GLTFLoader().load(model, obj=>{
 	mesh.material = material;
 	//mesh.geometry.computeVertexNormalsFine()
 
-	hand.rotation.set(-0.14, 0.17, 0);
+	hand.rotation.set(-0.25, 0.32, 0);
 	hand.position.y = -4.6
 
 	requestAnimationFrame(anim)
@@ -95,18 +108,20 @@ function anim(t) {
 }
 
 //ajustment
-onmousemove=e=>{
+canvas.onmousemove=e=>{
     if (e.which==1) {
         hand.rotation.x += e.movementY*.01
         hand.rotation.y += e.movementX*.01
     }
     if (e.which>1) {
-        hand.position.x += e.movementX*.03
-        hand.position.z += e.movementY*.03*(4-e.which)
-        if (e.which ==3) hand.position.y -= e.movementY*.03
+        scene.rotation.x += e.movementY*.01
+        scene.rotation.y += e.movementX*.01
+        // hand.position.x += e.movementX*.03
+        // hand.position.z += e.movementY*.03*(4-e.which)
+        // if (e.which ==3) hand.position.y -= e.movementY*.03
     }
 }
-onwheel=e=>{
+canvas.onwheel=e=>{
 	camera.zoom *= 1 - e.deltaY*.001;
 	camera.updateProjectionMatrix()
 }
